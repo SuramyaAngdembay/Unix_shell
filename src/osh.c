8bfd85e6f5388a506c54782fd8cc3622c488640e
Suramya Angdembay <sralimbu@gmail.com>
2025-04-17 14:56:39 -0500

    history function in shell

--- src/osh.c
+++ src/osh.c
@@ -8,9 +8,10 @@
 #define MAX_LINE 80 /* Max command length */
 
 int main(void) {
-    char *args[MAX_LINE / 2 + 1]; /* Command arguments (max 40 tokens + NULL) */
-    char input[MAX_LINE];         /* Buffer for input */
-    int should_run = 1;           /* Shell loop control */
+    char *args[MAX_LINE / 2 + 1];     /* Command arguments */
+    char input[MAX_LINE];             /* Buffer for input */
+    char last_command[MAX_LINE] = ""; /* History buffer */
+    int should_run = 1;               /* Shell loop control */
 
     while (should_run) {
         printf("osh> ");
@@ -24,8 +25,21 @@ int main(void) {
 
         /* Remove trailing newline */
         size_t len = strlen(input);
-        if (input[len - 1] == '\n') {
+        if (len > 0 && input[len - 1] == '\n') {
             input[len - 1] = '\0';
+        }
+
+        /* Handle "!!" history feature */
+        if (strcmp(input, "!!") == 0) {
+            if (strlen(last_command) == 0) {
+                printf("No commands in history.\n");
+                continue;
+            } else {
+                printf("%s\n", last_command);  // Echo the previous command
+                strcpy(input, last_command);   // Use it as input
+            }
+        } else {
+            strcpy(last_command, input); // Save this command to history
         }
 
         /* Check for "exit" command */
@@ -36,8 +50,8 @@ int main(void) {
 
         /* Parse input into arguments */
         int i = 0;
-        char *token = strtok(input, " ");
         int background = 0;
+        char *token = strtok(input, " ");
 
         while (token != NULL) {
             if (strcmp(token, "&") == 0) {
@@ -48,6 +62,18 @@ int main(void) {
             token = strtok(NULL, " ");
         }
         args[i] = NULL; /* Null-terminate the argument array */
+
+        /* Handle built-in "cd" command */
+        if (args[0] && strcmp(args[0], "cd") == 0) {
+            if (args[1] == NULL) {
+                fprintf(stderr, "osh: expected argument to \"cd\"\n");
+            } else {
+                if (chdir(args[1]) != 0) {
+                    perror("osh");
+                }
+            }
+            continue;
+        }
 
         /* Fork a child process */
         pid_t pid = fork();
